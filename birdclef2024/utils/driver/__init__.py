cca780e331136588e7a2b4babb082855c2528cf7
tky823 <tky823.main+github@gmail.com>
2024-05-23 22:41:32 +0900

    [src] Add driver for BirdCLEF2024

---
+++ birdclef2024/utils/driver/__init__.py
@@ -0,0 +1,5 @@
+from .base import BaseTrainer
+
+__all__ = [
+    "BaseTrainer",
+]
---
+++ birdclef2024/utils/driver/base.py
@@ -0,0 +1,33 @@
+import os
+
+from audyn.utils.driver import BaseTrainer as _BaseTrainer
+from huggingface_hub import upload_file
+
+_token = os.getenv("HUGGINGFACE_TOKEN")
+_repo_id = os.getenv("HUGGINGFACE_REPO_ID")
+
+
+class BaseTrainer(_BaseTrainer):
+    def save_checkpoint(self, save_path: str) -> None:
+        super().save_checkpoint(save_path)
+
+        upload_checkpoint(save_path)
+
+
+def upload_checkpoint(path: str) -> None:
+    cwd = os.getcwd()
+    recipe_name = os.path.basename(cwd)
+    path_in_repo = os.path.join("recipes", recipe_name, path)
+    repo_type = "model"
+
+    try:
+        upload_file(
+            path_or_fileobj=path,
+            path_in_repo=path_in_repo,
+            repo_id=_repo_id,
+            token=_token,
+            repo_type=repo_type,
+        )
+    except Exception:
+        # give up uploading
+        pass
