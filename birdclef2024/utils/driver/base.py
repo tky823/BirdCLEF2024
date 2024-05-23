396c2e48031010297b39778af3f3e9a69481ea40
tky823 <tky823.main+github@gmail.com>
2024-05-23 23:02:17 +0900

    [src] Show uploading log in driver

--- birdclef2024/utils/driver/base.py
+++ birdclef2024/utils/driver/base.py
@@ -11,23 +11,22 @@ class BaseTrainer(_BaseTrainer):
     def save_checkpoint(self, save_path: str) -> None:
         super().save_checkpoint(save_path)
 
-        upload_checkpoint(save_path)
+        self.upload_checkpoint(save_path)
 
+    def upload_checkpoint(self, path: str) -> None:
+        cwd = os.getcwd()
+        recipe_name = os.path.basename(cwd)
+        path_in_repo = os.path.join("recipes", recipe_name, path)
+        repo_type = "model"
 
-def upload_checkpoint(path: str) -> None:
-    cwd = os.getcwd()
-    recipe_name = os.path.basename(cwd)
-    path_in_repo = os.path.join("recipes", recipe_name, path)
-    repo_type = "model"
-
-    try:
-        upload_file(
-            path_or_fileobj=path,
-            path_in_repo=path_in_repo,
-            repo_id=_repo_id,
-            token=_token,
-            repo_type=repo_type,
-        )
-    except Exception:
-        # give up uploading
-        pass
+        try:
+            upload_file(
+                path_or_fileobj=path,
+                path_in_repo=path_in_repo,
+                repo_id=_repo_id,
+                token=_token,
+                repo_type=repo_type,
+            )
+        except Exception:
+            # give up uploading
+            self.logger.info(f"Failed in uploading {path_in_repo}")
