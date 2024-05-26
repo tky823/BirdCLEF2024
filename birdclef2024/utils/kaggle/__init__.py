6a4554e4e5d940e94f45759f6be1a04785d6bb13
tky823 <tky823.main+github@gmail.com>
2024-05-26 18:59:32 +0900

    [src] Support non-defined secrets

--- birdclef2024/utils/kaggle/__init__.py
+++ birdclef2024/utils/kaggle/__init__.py
@@ -3,8 +3,10 @@ from typing import Optional
 
 try:
     from kaggle_secrets import UserSecretsClient
+    from kaggle_web_client import BackendError
 except ImportError:
     UserSecretsClient = None
+    BackendError = None
 
 __all__ = [
     "load_huggingface_token",
@@ -13,20 +15,36 @@ __all__ = [
 
 
 def load_huggingface_token() -> Optional[str]:
+    token = None
+
     if UserSecretsClient is not None:
         user_secrets = UserSecretsClient()
-        token = user_secrets.get_secret("HUGGINGFACE_TOKEN")
-    else:
+
+        try:
+            token = user_secrets.get_secret("HUGGINGFACE_TOKEN")
+        except BackendError:
+            # if HUGGINGFACE_TOKEN is not defined
+            pass
+
+    if token is None:
         token = os.getenv("HUGGINGFACE_TOKEN")
 
     return token
 
 
 def load_huggingface_repo_id() -> Optional[str]:
+    repo_id = None
+
     if UserSecretsClient is not None:
         user_secrets = UserSecretsClient()
-        repo_id = user_secrets.get_secret("HUGGINGFACE_REPO_ID")
-    else:
+
+        try:
+            repo_id = user_secrets.get_secret("HUGGINGFACE_REPO_ID")
+        except BackendError:
+            # if HUGGINGFACE_REPO_ID is not defined
+            pass
+
+    if repo_id is None:
         repo_id = os.getenv("HUGGINGFACE_REPO_ID")
 
     return repo_id
