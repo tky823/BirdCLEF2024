1baa4c5006fb505849be5bc669c374ce8c75e60c
tky823 <tky823.main+github@gmail.com>
2024-05-29 00:20:44 +0900

    [src] Add composer and collator for inference

--- birdclef2024/utils/data/__init__.py
+++ birdclef2024/utils/data/__init__.py
@@ -1,5 +1,9 @@
-from .collator import BirdCLEF2024BaselineCollator
-from .composer import BirdCLEF2024AudioComposer, BirdCLEF2024PrimaryLabelComposer
+from .collator import BirdCLEF2024BaselineCollator, BirdCLEF2024ChunkingCollator
+from .composer import (
+    BirdCLEF2024AudioChunkingComposer,
+    BirdCLEF2024AudioComposer,
+    BirdCLEF2024PrimaryLabelComposer,
+)
 from .dataset import (
     BirdCLEF2024AudioDataset,
     BirdCLEF2024PrimaryLabelDataset,
@@ -14,5 +18,7 @@ __all__ = [
     "BirdCLEF2024WeightedRandomSampler",
     "BirdCLEF2024PrimaryLabelComposer",
     "BirdCLEF2024AudioComposer",
+    "BirdCLEF2024AudioChunkingComposer",
     "BirdCLEF2024BaselineCollator",
+    "BirdCLEF2024ChunkingCollator",
 ]
