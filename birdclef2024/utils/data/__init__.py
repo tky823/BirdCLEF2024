fadf7f65733b5d3014e10b967dbdd466acc549d4
tky823 <tky823.main+github@gmail.com>
2024-05-31 22:31:28 +0900

    [src] Fix names of modules

--- birdclef2024/utils/data/__init__.py
+++ birdclef2024/utils/data/__init__.py
@@ -1,8 +1,9 @@
-from .collator import BirdCLEF2024BaselineCollator, BirdCLEF2024ChunkingCollator
+from .collator import BirdCLEF2024AudioChunkingCollator, BirdCLEF2024BaselineCollator
 from .composer import (
     BirdCLEF2024AudioChunkingComposer,
     BirdCLEF2024AudioComposer,
     BirdCLEF2024PrimaryLabelComposer,
+    BirdCLEF2024SharedAudioComposer,
 )
 from .dataset import (
     BirdCLEF2024AudioDataset,
@@ -19,6 +20,7 @@ __all__ = [
     "BirdCLEF2024PrimaryLabelComposer",
     "BirdCLEF2024AudioComposer",
     "BirdCLEF2024AudioChunkingComposer",
+    "BirdCLEF2024SharedAudioComposer",
     "BirdCLEF2024BaselineCollator",
-    "BirdCLEF2024ChunkingCollator",
+    "BirdCLEF2024AudioChunkingCollator",
 ]
