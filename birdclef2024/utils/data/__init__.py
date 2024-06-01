1f7922415ff8f82e623a650375bb7a22467a4884
tky823 <tky823.main+github@gmail.com>
2024-06-01 15:39:50 +0900

    [src] Add modules to birdclef2024.data

--- birdclef2024/utils/data/__init__.py
+++ birdclef2024/utils/data/__init__.py
@@ -3,11 +3,13 @@ from .composer import (
     BirdCLEF2024AudioChunkingComposer,
     BirdCLEF2024AudioComposer,
     BirdCLEF2024PrimaryLabelComposer,
+    BirdCLEF2024PrimaryLabelDistillationComposer,
     BirdCLEF2024SharedAudioComposer,
 )
 from .dataset import (
     BirdCLEF2024AudioDataset,
     BirdCLEF2024PrimaryLabelDataset,
+    BirdCLEF2024PrimaryLabelDistillationDataset,
     WeightedBirdCLEF2024PrimaryLabelDataset,
 )
 from .sampler import BirdCLEF2024WeightedRandomSampler
@@ -16,8 +18,10 @@ __all__ = [
     "BirdCLEF2024PrimaryLabelDataset",
     "BirdCLEF2024AudioDataset",
     "WeightedBirdCLEF2024PrimaryLabelDataset",
+    "BirdCLEF2024PrimaryLabelDistillationDataset",
     "BirdCLEF2024WeightedRandomSampler",
     "BirdCLEF2024PrimaryLabelComposer",
+    "BirdCLEF2024PrimaryLabelDistillationComposer",
     "BirdCLEF2024AudioComposer",
     "BirdCLEF2024AudioChunkingComposer",
     "BirdCLEF2024SharedAudioComposer",
