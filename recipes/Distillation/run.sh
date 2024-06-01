28e9b03a91ffdfab9a5aac81c628eb4556c72f52
tky823 <tky823.main+github@gmail.com>
2024-06-01 15:10:02 +0900

    [recipes] Update train.sh for distillation

---
+++ recipes/Distillation/run.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+stage=-1
+stop_stage=-1
+
+tag=""
+continue_from=""
+checkpoint=""
+submission_path=""
+
+exp_root="./exp"
+tensorboard_root="./tensorboard"
+
+data_root="../data"
+dump_root="dump"
+
+dump_format="birdclef2024"
+
+system="defaults"
+preprocess="birdclef2024"
+data="birdclef2024"
+train="birdclef2024distillation"
+test="defaults"
+model="birdclef2024distillation"
+optimizer="adam_student"
+lr_scheduler="cos_anneal_student"
+criterion="birdclef2024_distillation"
+
+. ../_common/parse_options.sh || exit 1;
+
+if [ -f .env ]; then
+    set -a
+    source .env
+    set +a
+fi
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    echo "Stage -1: Download dataset"
+
+    echo "Please download dataset from official page and place it to ${data_root}."
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    echo "Stage 0: Preprocess dataset"
+
+    (
+        . ./preprocess.sh \
+        --stage 1 \
+        --stop-stage 2 \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --preprocess "${preprocess}" \
+        --data "${data}"
+    )
+fi
+
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo "Stage 1: Train EfficientNet"
+
+    (
+        . ./train.sh \
+        --tag "${tag}" \
+        --continue-from "${continue_from}" \
+        --exp-root "${exp_root}" \
+        --tensorboard-root "${tensorboard_root}" \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --system "${system}" \
+        --preprocess "${preprocess}" \
+        --data "${data}" \
+        --train "${train}" \
+        --model "${model}" \
+        --optimizer "${optimizer}" \
+        --lr-scheduler "${lr_scheduler}" \
+        --criterion "${criterion}"
+    )
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    echo "Stage 2: Preprocess test dataset"
+
+    (
+        . ./preprocess.sh \
+        --stage 3 \
+        --stop-stage 4 \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --preprocess "${preprocess}" \
+        --data "${data}"
+    )
+fi
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+    echo "Stage 3: Infer by EfficientNet"
+
+    (
+        . ./test.sh \
+        --tag "${tag}" \
+        --checkpoint "${checkpoint}" \
+        --exp-root "${exp_root}" \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --system "${system}" \
+        --preprocess "${preprocess}" \
+        --data "${data}" \
+        --train "${train}" \
+        --test "${test}" \
+        --model "${model}"
+    )
+fi
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+    echo "Stage 4: Submit estimation."
+
+    (
+        . ./submit.sh \
+        --tag "${tag}" \
+        --submission-path "${submission_path}" \
+        --exp-root "${exp_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --system "${system}" \
+        --preprocess "${preprocess}" \
+        --data "${data}" \
+        --train "${train}" \
+        --test "${test}" \
+        --model "${model}"
+    )
+fi
--- recipes/Distillation/train.sh
+++ recipes/Distillation/train.sh
@@ -1,1 +1,75 @@
-../Baseline/train.sh
+#!/bin/bash
+
+data_root="../data"
+dump_root="./dump"
+exp_root="./exp"
+tensorboard_root="./tensorboard"
+
+tag=""
+continue_from=""
+
+dump_format="birdclef2024"
+
+system="defaults"
+preprocess="birdclef2024"
+data="birdclef2024"
+train="birdclef2024baseline_birdclef2024"
+model="birdclef2024baseline"
+optimizer="birdclef2024baseline"
+lr_scheduler="cos_anneal"
+criterion="birdclef2024"
+
+. ../_common/parse_options.sh || exit 1;
+
+dump_dir="${dump_root}/${data}"
+list_dir="${dump_dir}/list"
+feature_dir="${dump_dir}/feature"
+
+if [ -z "${tag}" ]; then
+    tag=$(date +"%Y%m%d-%H%M%S")
+fi
+
+exp_dir="${exp_root}/${tag}"
+tensorboard_dir="${tensorboard_root}/${tag}"
+
+cmd=$(
+    python ../_common/parse_run_command.py \
+    --config-dir "./conf" \
+    hydra.run.dir="./log/$(date +"%Y%m%d-%H%M%S")" \
+    system="${system}"
+)
+
+train_labeled_list_path="${list_dir}/train.txt"
+train_unlabeled_list_path="${list_dir}/unlabeled_train.txt"
+validation_labeled_list_path="${list_dir}/validation.txt"
+validation_unlabeled_list_path="${list_dir}/unlabeled_validation.txt"
+
+if [ "${dump_format}" = "birdclef2024" ]; then
+    train_feature_dir="${data_root}/birdclef-2024"
+    validation_feature_dir="${data_root}/birdclef-2024"
+else
+    train_feature_dir="${feature_dir}/train"
+    validation_feature_dir="${feature_dir}/validation"
+fi
+
+${cmd} ./local/train.py \
+--config-dir "./conf" \
+hydra.run.dir="${exp_dir}/log/$(date +"%Y%m%d-%H%M%S")" \
+system="${system}" \
+preprocess="${preprocess}" \
+data="${data}" \
+train="${train}" \
+model="${model}" \
+optimizer="${optimizer}" \
+lr_scheduler="${lr_scheduler}" \
+criterion="${criterion}" \
+preprocess.dump_format="${dump_format}" \
+train.dataset.train.labeled_list_path="${train_labeled_list_path}" \
+train.dataset.train.unlabeled_list_path="${train_unlabeled_list_path}" \
+train.dataset.train.feature_dir="${train_feature_dir}" \
+train.dataset.validation.labeled_list_path="${validation_labeled_list_path}" \
+train.dataset.validation.unlabeled_list_path="${validation_unlabeled_list_path}" \
+train.dataset.validation.feature_dir="${validation_feature_dir}" \
+train.resume.continue_from="${continue_from}" \
+train.output.exp_dir="${exp_dir}" \
+train.output.tensorboard_dir="${tensorboard_dir}"
