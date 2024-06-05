e3b42372f2ef86ba513423afdbfe4d31818b8574
tky823 <tky823.main+github@gmail.com>
2024-06-05 22:58:00 +0900

    [recipes] Fix preprocessing scripts

--- recipes/MultiDataset/preprocess_2021.sh
+++ recipes/MultiDataset/preprocess_2021.sh
@@ -17,9 +17,9 @@ data="birdclef2024"
 
 . ../_common/parse_options.sh || exit 1;
 
-birdclef2022_dataroot="${data_root}/birdclef-2022"
-csv_path="${birdclef2022_dataroot}/train_metadata.csv"
-audio_root="${birdclef2022_dataroot}/train_audio"
+birdclef2021_dataroot="${data_root}/birdclef-2021"
+csv_path="${birdclef2021_dataroot}/train_metadata.csv"
+audio_root="${birdclef2021_dataroot}/train_audio"
 
 dump_dir="${dump_root}/${data}"
 list_dir="${dump_dir}/list"
@@ -30,48 +30,45 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 
     mkdir -p "${list_dir}"
 
-    existing_list_path="${list_dir}/additional.txt"
+    subset="train_2021"
+    list_path="${list_dir}/${subset}.txt"
+    existing_list_path="${list_dir}/additional_train.txt"
     birdclef2024_train_list_path="${list_dir}/train.txt"
     birdclef2024_validation_list_path="${list_dir}/validation.txt"
 
-    for subset in "train" "validation"; do
-        list_path="${list_dir}/${subset}_2021.txt"
+    python ./local/select_samples.py \
+    --config-dir "./conf" \
+    hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
+    preprocess="${preprocess}" \
+    data="${data}" \
+    preprocess.dump_format="${dump_format}" \
+    preprocess.list_path="${list_path}" \
+    preprocess.csv_path="${csv_path}" \
+    preprocess.audio_root="${audio_root}" \
+    preprocess.subset="${subset}" \
+    preprocess.existing_list_path="${existing_list_path}" \
+    preprocess.birdclef2024_train_list_path="${birdclef2024_train_list_path}" \
+    preprocess.birdclef2024_validation_list_path="${birdclef2024_validation_list_path}"
 
-        python ./local/save_2021_list.py \
-        --config-dir "./conf" \
-        hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
-        preprocess="${preprocess}" \
-        data="${data}" \
-        preprocess.dump_format="${dump_format}" \
-        preprocess.list_path="${list_path}" \
-        preprocess.csv_path="${csv_path}" \
-        preprocess.audio_root="${audio_root}" \
-        preprocess.subset="${subset}" \
-        preprocess.existing_list_path="${existing_list_path}" \
-        preprocess.birdclef2024_train_list_path="${birdclef2024_train_list_path}" \
-        preprocess.birdclef2024_validation_list_path="${birdclef2024_validation_list_path}"
-
-        cat "${list_path}" >> "${existing_list_path}"
-    done
+    cat "${list_path}" >> "${existing_list_path}"
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     echo "Preprocess stage 2: Save features"
 
-    for subset in "train" "validation"; do
-        subset_list_path="${list_dir}/${subset}_2021.txt"
-        subset_feature_dir="${feature_dir}/${subset}_2021"
+    subset="train_2021"
+    subset_list_path="${list_dir}/${subset}.txt"
+    subset_feature_dir="${feature_dir}/${subset}"
 
-        python ../_common/local/save_features.py \
-        --config-dir "./conf" \
-        hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
-        preprocess="${preprocess}" \
-        data="${data}" \
-        preprocess.dump_format="${dump_format}" \
-        preprocess.list_path="${subset_list_path}" \
-        preprocess.feature_dir="${subset_feature_dir}" \
-        preprocess.csv_path="${csv_path}" \
-        preprocess.audio_root="${audio_root}" \
-        preprocess.subset="${subset}"
-    done
+    python ../_common/local/save_features.py \
+    --config-dir "./conf" \
+    hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
+    preprocess="${preprocess}" \
+    data="${data}" \
+    preprocess.dump_format="${dump_format}" \
+    preprocess.list_path="${subset_list_path}" \
+    preprocess.feature_dir="${subset_feature_dir}" \
+    preprocess.csv_path="${csv_path}" \
+    preprocess.audio_root="${audio_root}" \
+    preprocess.subset="${subset}"
 fi
--- recipes/MultiDataset/preprocess_2023.sh
+++ recipes/MultiDataset/preprocess_2023.sh
@@ -30,48 +30,45 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
 
     mkdir -p "${list_dir}"
 
-    existing_list_path="${list_dir}/additional.txt"
+    subset="train_2023"
+    list_path="${list_dir}/${subset}.txt"
+    existing_list_path="${list_dir}/additional_train.txt"
     birdclef2024_train_list_path="${list_dir}/train.txt"
     birdclef2024_validation_list_path="${list_dir}/validation.txt"
 
-    for subset in "train" "validation"; do
-        list_path="${list_dir}/${subset}_2023.txt"
+    python ./local/select_samples.py \
+    --config-dir "./conf" \
+    hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
+    preprocess="${preprocess}" \
+    data="${data}" \
+    preprocess.dump_format="${dump_format}" \
+    preprocess.list_path="${list_path}" \
+    preprocess.csv_path="${csv_path}" \
+    preprocess.audio_root="${audio_root}" \
+    preprocess.subset="${subset}" \
+    preprocess.existing_list_path="${existing_list_path}" \
+    preprocess.birdclef2024_train_list_path="${birdclef2024_train_list_path}" \
+    preprocess.birdclef2024_validation_list_path="${birdclef2024_validation_list_path}"
 
-        python ./local/save_2023_list.py \
-        --config-dir "./conf" \
-        hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
-        preprocess="${preprocess}" \
-        data="${data}" \
-        preprocess.dump_format="${dump_format}" \
-        preprocess.list_path="${list_path}" \
-        preprocess.csv_path="${csv_path}" \
-        preprocess.audio_root="${audio_root}" \
-        preprocess.subset="${subset}" \
-        preprocess.existing_list_path="${existing_list_path}" \
-        preprocess.birdclef2024_train_list_path="${birdclef2024_train_list_path}" \
-        preprocess.birdclef2024_validation_list_path="${birdclef2024_validation_list_path}"
-
-        cat "${list_path}" >> "${existing_list_path}"
-    done
+    cat "${list_path}" >> "${existing_list_path}"
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     echo "Preprocess stage 2: Save features"
 
-    for subset in "train" "validation"; do
-        subset_list_path="${list_dir}/${subset}_2023.txt"
-        subset_feature_dir="${feature_dir}/${subset}_2023"
+    subset="train_2023"
+    subset_list_path="${list_dir}/${subset}.txt"
+    subset_feature_dir="${feature_dir}/${subset}"
 
-        python ../_common/local/save_features.py \
-        --config-dir "./conf" \
-        hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
-        preprocess="${preprocess}" \
-        data="${data}" \
-        preprocess.dump_format="${dump_format}" \
-        preprocess.list_path="${subset_list_path}" \
-        preprocess.feature_dir="${subset_feature_dir}" \
-        preprocess.csv_path="${csv_path}" \
-        preprocess.audio_root="${audio_root}" \
-        preprocess.subset="${subset}"
-    done
+    python ../_common/local/save_features.py \
+    --config-dir "./conf" \
+    hydra.run.dir="${log_dir}/$(date +"%Y%m%d-%H%M%S")" \
+    preprocess="${preprocess}" \
+    data="${data}" \
+    preprocess.dump_format="${dump_format}" \
+    preprocess.list_path="${subset_list_path}" \
+    preprocess.feature_dir="${subset_feature_dir}" \
+    preprocess.csv_path="${csv_path}" \
+    preprocess.audio_root="${audio_root}" \
+    preprocess.subset="${subset}"
 fi
