abac3579985db6dfb5708a7c2882fbe26ab10595
tky823 <tky823.main+github@gmail.com>
2024-06-06 21:47:32 +0900

    [recipes] Add stage 2 of MultiDatasetPretrain

--- recipes/MultiDatasetPretrain/run.sh
+++ recipes/MultiDatasetPretrain/run.sh
@@ -80,3 +80,27 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         --criterion "${criterion}"
     )
 fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    echo "Stage 2: Finetune EfficientNet"
+
+    (
+        . ./finetune.sh \
+        --tag "${tag}" \
+        --continue-from "${continue_from}" \
+        --checkpoint "${checkpoint}" \
+        --exp-root "${exp_root}" \
+        --tensorboard-root "${tensorboard_root}" \
+        --data-root "${data_root}" \
+        --dump-root "${dump_root}" \
+        --dump-format "${dump_format}" \
+        --system "${system}" \
+        --preprocess "${preprocess}" \
+        --data "${data}" \
+        --train "${train}" \
+        --model "${model}" \
+        --optimizer "${optimizer}" \
+        --lr-scheduler "${lr_scheduler}" \
+        --criterion "${criterion}"
+    )
+fi
