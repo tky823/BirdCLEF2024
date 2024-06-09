6b076cb621303fb581902fa7cd971accd41a70c1
tky823 <tky823.main+github@gmail.com>
2024-06-09 17:05:45 +0900

    [src] Import ArgmaxAverageAudioGenerator

--- birdclef2024/utils/driver/__init__.py
+++ birdclef2024/utils/driver/__init__.py
@@ -1,4 +1,10 @@
-from .base import AverageAudioGenerator, BaseGenerator, BaseTrainer, SharedAudioGenerator
+from .base import (
+    ArgmaxAverageAudioGenerator,
+    AverageAudioGenerator,
+    BaseGenerator,
+    BaseTrainer,
+    SharedAudioGenerator,
+)
 from .distillation import StudentTrainer
 
 __all__ = [
