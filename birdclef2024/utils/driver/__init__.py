8fbdd5da12eb801e8f19667e99f6374437d4d17f
tky823 <tky823.main+github@gmail.com>
2024-06-09 15:51:24 +0900

    [src] Add AverageAudioGenerator

--- birdclef2024/utils/driver/__init__.py
+++ birdclef2024/utils/driver/__init__.py
@@ -1,9 +1,10 @@
-from .base import BaseGenerator, BaseTrainer, SharedAudioGenerator
+from .base import AverageAudioGenerator, BaseGenerator, BaseTrainer, SharedAudioGenerator
 from .distillation import StudentTrainer
 
 __all__ = [
     "BaseTrainer",
     "BaseGenerator",
     "SharedAudioGenerator",
+    "AverageAudioGenerator",
     "StudentTrainer",
 ]
